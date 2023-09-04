1607b67eda9bb3ed5ffb683b3f61de726680a13c
Mishka <mikheil.zhvania@makingscience.com>
2023-09-04 14:05:38 +0400

    last update

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
